74852bea67839500dd4b13183c65760cb5c26f81
Musa Maphutha <josephmaphutha17@gmail.com>
2026-04-02 17:23:38 +0200

    feat: Rendered content in the DOM using grid for both mobile and larger screens

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-const journalData = [
+export const journalData = [
     {
         image: "./images/blackjack.png",
         title: "Blackjack",
---
+++ index.js
@@ -0,0 +1,35 @@
+import { journalData } from "./data.js"
+
+const time = document.getElementById("date")
+const contentHtml = document.getElementById("content")
+
+function getRealTime(){
+    const date = new Date()
+    const formarttedDate = date.toLocaleDateString("en-US")
+    
+    return formarttedDate
+}
+
+
+time.textContent = getRealTime()
+
+function renderHtml(){
+    let renderData = ""
+    journalData.forEach(data => {
+        renderData += `
+        <a href="${data.link}" aria-label="Visit link to my ${data.title} project" class="data" target="blank">
+            <div>
+                <img src=${data.image} alt="A picture of ${data.title}" class="images">
+                <p>${getRealTime()}</p>
+                <h1>${data.title}</h1>
+                <p>${data.description}
+            </div>
+
+        </a>
+        ` 
+
+    })
+    contentHtml.innerHTML = renderData
+}
+
+renderHtml()
