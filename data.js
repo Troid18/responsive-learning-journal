0f576048a4e5961f2de8d65e2afdbabc2823a493
Musa Maphutha <josephmaphutha17@gmail.com>
2026-04-02 08:23:21 +0200

    feat: Added data object to render in the DOM later on.

---
+++ data.js
@@ -0,0 +1,45 @@
+const journalData = [
+    {
+        image: "./images/blackjack.png",
+        title: "Blackjack",
+        description: "This is my first frontend project. I started learning Javascript using this project.",
+        link: "https://github.com/Troid18/Blackjack"
+
+    },
+    {
+        image: "./images/password.jpg",
+        title: "Password Generator",
+        description: "This is my second frontend project. I generated two random strong passwords since security has become a problem in the modern internet. I started to build using Figma with this one.",
+        link: "https://github.com/Troid18/https://github.com/Troid18/Password_Generator"
+
+    },
+    {
+        image: "./images/converter.png",
+        title: "Unit Converter",
+        description: "This is my third frontend project. I have implemented conversions using Vanilla javascript. It is a project that helped me understand the Mathematics of Javascript.",
+        link: "https://github.com/Troid18/https://github.com/Troid18/Unit-Converter"
+
+    },
+    {
+        image: "./images/tracker.png",
+        title: "Tab Tracker",
+        description: "This is my fourth frontend project. I learned how to build uding a monorepo. I built the chrome extension and web-app using firebase for real time database.",
+        link: "https://github.com/Troid18/https://github.com/Troid18/tab_tracker_monorepo"
+
+    },
+    {
+        image: "./images/twitter.jpg",
+        title: "Twitter Clone",
+        description: "This is my fifth frontend project. I created a comment section clone from twitter. It is too advance but i learned important concepts.",
+        link: "https://github.com/Troid18/https://github.com/Troid18/Twitter-Clone"
+
+    },
+    {
+        image: "./images/resturant.png",
+        title: "Resturant App",
+        description: "This is my sixth frontend project. I learnt more aboout DOM manipulation a lot with this project. It might not be that of a good looking project but it does its work.",
+        link: "https://github.com/Troid18/https://github.com/Troid18/Resturant-App"
+
+    },
+    
+]
